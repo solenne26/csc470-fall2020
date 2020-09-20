a0ebe26189ad2a6b99ac79341984a68cdbd52704
Solenne Smith <ss5070b@student.american.edu>
2020-09-20 18:48:46 -0400

    finished game

---
+++ exercises/game02/Assets/FryScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FryScript : MonoBehaviour
+{
+	public bool chaseGator = false;
+	GameObject gator;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+    	// Get a reference to the player (gator).
+		gator = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	// In the PlayerController script, we set chaseGator to true when the
+    	// gator collides with a fries.
+        if (chaseGator) {
+        	// Compute the vector to the gator and normalize it.
+        	// We do this by subtracting our position (the fry's) from the destination's
+        	// position (the gator).
+			Vector3 directionToGator = gator.transform.position - transform.position;
+			directionToGator = directionToGator.normalized;
+			
+			// Move toward the gator.
+			transform.Translate(directionToGator * Time.deltaTime * 6);
+		}
+	}
+}
---
+++ exercises/game02/Assets/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+	float speed = 10f;
+	float rotateSpeed = 100f;
+	int score = 0;
+
+	// This value is set in the Unity editor by dragging the Text object
+	// into the slot in the inspector.
+	public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+	void Update()
+	{
+		// Rotate on the y axis based on the hAxis value
+		// NOTE: If the player isn't pressing left or right, hAxis will be 0 and there 
+		// will be no rotation.
+		// NOTE: We add the modifer "Space.World" to make it so that the rotation
+		// works the way that we expect.
+		float hAxis = Input.GetAxis("Horizontal");
+		transform.Rotate(0, hAxis * rotateSpeed * Time.deltaTime, 0, Space.World);
+
+		// Move forward
+		//transform.position = transform.position + transform.forward * Time.deltaTime;
+		// NOTE: We add the modifer "Space.World" to make it so that the movement
+		// works the way that we expect (i.e. using the global coordinate system).
+		transform.Translate(transform.forward * Time.deltaTime * speed, Space.World);
+		
+		
+		// --- Simulating skateboarding ---
+		// Make speed go down over time.
+		if (speed > 0) {
+			speed -= 4 * Time.deltaTime;
+		}
+		// Make it so we move faster when we press space
+		if (Input.GetKeyDown(KeyCode.W)) {
+			speed += 8;
+		}
+		// Make sure speed doesn't get less than zero or greater than 15.
+		speed = Mathf.Clamp(speed, 0, 15);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		// "other" is a reference to the collider that we just collided with.
+		// Check to see if that object has been tagged with "fries" (you set tags
+		// at the top of the inspector when you have a game object selected.
+		if (other.gameObject.CompareTag("dragonflies")) {
+			// Get a reference to the FryScript on the fries that we just collided with
+			// FryScript fry = other.gameObject.GetComponent<FryScript>();
+
+			// if (!fry.chaseGator) {
+			// 	// Set the bool in the FryScript object to true causing the Fries
+			// 	// to start chasing the gator (see the Update function in FryScript).
+			// 	// Only do this if we aren't already chasing the gator.
+			// 	fry.chaseGator = true;
+
+			// 	// Destroy it in 10 seconds
+			// 	Destroy(other.gameObject, 10);
+			// }
+			Destroy(other.gameObject);
+		}
+	}
+	
+}
